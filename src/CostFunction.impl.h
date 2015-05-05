2ac7b93e5a5e2351fdc5e7feb73972daa7119dc8
Lene Preuss <lene.preuss@gmail.com>
2015-05-05 16:37:39 +0200

    added precondition to check arguments for cost function ctor

--- src/CostFunction.impl.h
+++ src/CostFunction.impl.h
@@ -6,11 +6,15 @@
 
 #include "viennacl/linalg/inner_prod.hpp"
 #include "viennacl/linalg/prod.hpp"
+#include "MatrixPrinter.h"
+#include "VectorPrinter.h"
 
 template <typename ScalarType>
 CostFunction<ScalarType>::CostFunction(
         const viennacl::matrix<ScalarType> &X,
-        const viennacl::vector<ScalarType> &y): X_(X), y_(y) { }
+        const viennacl::vector<ScalarType> &y): X_(X), y_(y) {
+    assert(y_.size() == X_.size2());
+}
 
 
 /**
@@ -24,6 +28,7 @@ function J = computeCostMulti(X, y, theta)
 template <typename ScalarType>
 viennacl::scalar<ScalarType> CostFunction<ScalarType>::operator()(const viennacl::vector<ScalarType> &theta) {
     assert(theta.size() == X_.size1());
+
     viennacl::vector<ScalarType> h_theta = viennacl::linalg::prod(trans(X_), theta);
     viennacl::vector<ScalarType> deviation = h_theta - y_;
     return viennacl::linalg::inner_prod(deviation, deviation);
