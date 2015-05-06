a66701b233f01d19b4ae770d873af640e059ac8e
Lene Preuss <lene.preuss@gmail.com>
2015-05-06 20:03:16 +0200

    more playing around with feature normalization - better but not yet correct

--- src/FeatureNormalize.h
+++ src/FeatureNormalize.h
@@ -11,6 +11,9 @@
 
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/matrix_proxy.hpp>
+#include "VectorPrinter.h"
+#include "MatrixPrinter.h"
+
 using namespace boost::numeric;
 
 template <typename ScalarType>
@@ -42,10 +45,8 @@ private:
     }
 
     ScalarType variance(const ublas::vector<ScalarType> &v) {
-        ublas::vector<ScalarType> mean_val = ublas::scalar_vector<ScalarType>(v.size(), mean(v));
-        ublas::vector<ScalarType> difference = v - mean_val;
-        for (auto x: difference) x *= x;
-        return mean(difference);
+        ublas::vector<ScalarType> sqdiff = element_prod(v, v);
+        return mean(sqdiff);
     }
 
     ScalarType stddev(const ublas::vector<ScalarType> &v) {
@@ -56,12 +57,25 @@ private:
         unsigned long len = blas_matrix_.size2();
         for (unsigned long i = 0; i < len; ++i) {
             ublas::vector<ScalarType> column = ublas::column(blas_matrix_, i);
+            VectorPrinter<ublas::vector<ScalarType>> pc(column);
+            pc.print("col");
             mu_(i) = mean(column);
+            std::cout << "mu: " << mu_(i) << std::endl;
             ublas::vector<ScalarType> normalized_column = ublas::column(blas_matrix_, i);
             normalized_column -= ScalarType(mu_(i)) * ublas::scalar_vector<ScalarType>(normalized_column.size(), 1.0);
+            VectorPrinter<ublas::vector<ScalarType>> pnc(normalized_column);
+            pnc.print("normalized 1");
             sigma_(i) = stddev(normalized_column);
+            std::cout << "sigma: " << sigma_(i) << std::endl;
             normalized_column /= sigma_(i);
+            VectorPrinter<ublas::vector<ScalarType>> pnc2(normalized_column);
+            pnc2.print("normalized 2");
+            for (unsigned j = 0; j < blas_matrix_.size1(); ++j) {
+                normalized_matrix_(j, i) = normalized_column(j);
+            }
         }
+        MatrixPrinter<ublas::matrix<ScalarType>> p(normalized_matrix_);
+        p.print("normalized");
 
     }
 
--- test/test_featurenormalize.cpp
+++ test/test_featurenormalize.cpp
@@ -3,12 +3,28 @@
 //
 
 #include "FeatureNormalize.h"
+#include "FileReader.h"
 
 #include <gtest/gtest.h>
 
 class FeatureNormalizeTest: public ::testing::Test {
+protected:
+    FeatureNormalizeTest(): mat_stream_(mat_data_) { }
+    const std::string mat_data_ = "3 2\n0.1 0.2\n1.0 2.0\n10.0 20.0";
+    std::stringstream mat_stream_;
 };
 
 TEST_F(FeatureNormalizeTest, RunsAtAll) {
-    ASSERT_TRUE(true);
+    viennacl::matrix<float> M = FileReader::read_matrix<float>(mat_stream_);
+    FeatureNormalize<float> F(M);
+}
+
+TEST_F(FeatureNormalizeTest, NormalizedMatrix) {
+    viennacl::matrix<float> M = FileReader::read_matrix<float>(mat_stream_);
+    MatrixPrinter<viennacl::matrix<float>> po(M);
+    po.print();
+    FeatureNormalize<float> F(M);
+    ublas::matrix<float> normalized = F.normalize();
+    MatrixPrinter<ublas::matrix<float>> pn(normalized);
+    pn.print();
 }
