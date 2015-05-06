096642296c418f9fc90e2ab87f28818a95dba8d3
Lene Preuss <lene.preuss@gmail.com>
2015-05-06 19:28:12 +0200

    added skeleton for feature normalize test

---
+++ test/test_featurenormalize.cpp
@@ -0,0 +1,14 @@
+//
+// Created by lene on 06.05.15.
+//
+
+#include "FeatureNormalize.h"
+
+#include <gtest/gtest.h>
+
+class FeatureNormalizeTest: public ::testing::Test {
+};
+
+TEST_F(FeatureNormalizeTest, RunsAtAll) {
+    ASSERT_TRUE(true);
+}
