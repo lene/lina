2c41928fa785f40299c23f68c233dd5926a11744
Lene Preuss <lene.preuss@gmail.com>
2015-05-07 13:56:39 +0200

    used utility fixture functions

--- test/test_costfunction.cpp
+++ test/test_costfunction.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "CostFunction.h"
+#include "Utilities.h"
 
 #include <gtest/gtest.h>
 
@@ -13,52 +14,20 @@ using namespace boost::numeric;
 
 class CostFunctionTest: public ::testing::Test {
 
-public:
-    template <typename Scalar, typename Container = std::vector<Scalar> >
-    ublas::matrix<float> umat(const Container &values, int size1, int size2) {
-        ublas::matrix<float> X(size1, size2);
-        for (int i = 0; i < size1; ++i)
-            for(int j = 0; j < size2; ++j)
-                X(i, j) = values[i*size2+j];
-        return X;
-    }
-
-    template <typename Scalar, typename Container = std::vector<Scalar> >
-    void vmat(viennacl::matrix<Scalar> &mat, const Container &values, int size1, int size2) {
-        viennacl::copy(umat<Scalar>(values, size1, size2), mat);
-    }
-
-    template <typename Scalar, typename Container = std::vector<Scalar> >
-    ublas::vector<Scalar> uvec(const Container &values, int size) {
-        ublas::vector<Scalar> v(size);
-        for (int i = 0; i < size; ++i)
-            v(i) = values[i];
-        return v;
-    }
-
-    template <typename Scalar, typename Container = std::vector<Scalar> >
-    void vvec(viennacl::vector<Scalar> &vec, const Container &values, int size) {
-        viennacl::copy(uvec<Scalar>(values, size), vec);
-    }
-
 protected:
-    CostFunctionTest():
-            X_(viennacl::matrix<float>(3, 2)),
-            y_(viennacl::vector<float>(3)),
-            theta_(viennacl::vector<float>(2)) { }
 
     virtual void SetUp() {
-        std::vector<float> data = { 1.f, 1.f,  1.f, 0.f,  1.f, 0.f };
-        vmat<float>(X_, data, 3, 2);
-        data = { 1.f, 0.f, 0.f };
-        vvec<float>(y_, data, 3);
-        data = { 0.f, 0.f, 0.f };
-        vvec<float>(theta_, data, 2);
+        X_ = Utilities::matrixFixture(x_data_);
+        y_ = Utilities::vectorFixture(y_data_);
+        theta_ = Utilities::vectorFixture(theta_data);
     }
 
     viennacl::matrix<float> X_;
     viennacl::vector<float> y_, theta_;
 
+    const std::string x_data_ = "3 2 1 1 1 0 1 0";
+    const std::string y_data_ = "3 1 0 0";
+    const std::string theta_data = "2 0 0";
 };
 
 TEST_F(CostFunctionTest, WorksAtAll) {
@@ -95,16 +64,10 @@ TEST_F(CostFunctionTest, CostFunctionEvaluates) {
 
 TEST_F(CostFunctionTest, CostFunctionIdealTheta) {
     CostFunction<float> cost(X_, y_);
-    std::vector<float> data = { 0.f, 1.f, 0.f };
-    vvec(theta_, data, 3);
-    ASSERT_EQ(0.f, cost(theta_));
+    ASSERT_EQ(0.f, cost(Utilities::vectorFixture("2 0 1")));
 }
 
 TEST_F(CostFunctionTest, CostFunctionBadTheta) {
     CostFunction<float> cost(X_, y_);
-    float cost1 = cost(theta_);
-    std::vector<float> data = { 0.f, 10.f, 0.f };
-    vvec(theta_, data, 3);
-    float cost2 = cost(theta_);
-    ASSERT_LT(cost1, cost2);
+    ASSERT_LT(cost(theta_), cost(Utilities::vectorFixture("2 0 10")));
 }
